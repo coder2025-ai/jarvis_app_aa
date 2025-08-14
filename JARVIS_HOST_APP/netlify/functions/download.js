36b6d62e0bba2f745d0742c813ec3c1394fc6f7d
coder2025-ai <coder20252026@gmail.com>
2025-08-14 19:38:08 +0530

    Update download.js

--- JARVIS_HOST_APP/netlify/functions/download.js
+++ JARVIS_HOST_APP/netlify/functions/download.js
@@ -8,7 +8,7 @@ exports.handler = async (event, context) => {
   }
 
   // If the user is logged in, perform the redirect
-  const downloadUrl = 'https://www.dropbox.com/scl/fi/woeg78hhpxcz7zkvzehgl/jarvis.exe?rlkey=pp17u2yji0taovaxude4lf8lp&st=9ln25qzi&dl=0';
+  const downloadUrl = 'https://drive.google.com/uc?export=download&id=1S_BSHw0wCW9gozt9Q3qeIIoXKY6pLlIk';
 
   return {
     statusCode: 302, // 302 is the status code for a temporary redirect
